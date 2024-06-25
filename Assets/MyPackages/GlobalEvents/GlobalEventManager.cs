97e9ba36cc22207ebbd77166cd403f2d78725c8d
Cheesemcpuff <joshuahall98@live.com>
2024-06-25 20:26:58 +0100

    script clean

--- Assets/MyPackages/GlobalEvents/GlobalEventManager.cs
+++ Assets/MyPackages/GlobalEvents/GlobalEventManager.cs
@@ -17,7 +17,6 @@ public class GlobalEventManager : MonoBehaviour
         globalEventManagerInstance = this;
     }
 
-    // Update is called once per frame
     void Update()
     {
         UpdateEvent?.Invoke();
