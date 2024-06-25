2c4ec98d6d4d4e1628b9104da364305c3e1b3ad4
Cheesemcpuff <joshuahall98@live.com>
2024-06-25 19:41:04 +0100

    global event manager class created to have update methods you can subscribe to

---
+++ Assets/MyPackages/GlobalEvents/GlobalEventManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalEventManager : MonoBehaviour
+{
+
+    public static GlobalEventManager globalEventManagerInstance;
+
+    public event Action UpdateEvent;
+    public event Action FixedUpdateEvent;
+    public event Action LateUpdateEvent;
+
+    private void Awake()
+    {
+        globalEventManagerInstance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateEvent?.Invoke();
+    }
+
+    private void FixedUpdate()
+    {
+        FixedUpdateEvent?.Invoke();
+    }
+
+    private void LateUpdate()
+    {
+        LateUpdateEvent?.Invoke();
+    }
+}
